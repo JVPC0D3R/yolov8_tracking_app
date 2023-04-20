9e3b0358d3d9999568001d0bb3119ce4051ce735
jvp <villaplazajaime@gmail.com>
2023-04-20 15:00:25 +0200

    Tracker Manager added

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from ultralytics import YOLO
 import supervision as sv
 import cv2
 
+from utils.tracker_manager import TrackerManager
+
 from modules.view import draw_trasnparent_bbox, draw_label, resize_img
 
 # Load the model
-model = YOLO("yolov8n.pt") 
+model = YOLO("yolov8n.pt")
+
+# Init tracker manager
+tm = TrackerManager()
 
 # Use the model
 for result  in model.track(source =0, show = False, stream = True):
@@ -21,6 +26,9 @@ for result  in model.track(source =0, show = False, stream = True):
         
         detections.tracker_id = result.boxes.id.cpu().numpy().astype(int)
 
+        # List to store track outputs
+        track_outputs = []
+
         # Get object information
         for bbox, _, conf, class_id, tracker_id in detections:
             
@@ -30,9 +38,13 @@ for result  in model.track(source =0, show = False, stream = True):
             # Get label
             label = f'#{tracker_id} {model.model.names[class_id]}'
 
+            track_outputs.append([tracker_id, [x0, y0, x1, y1]])
+
             # Draw transparent bounding box and write label
             draw_label(frame, label, int(x0), int(y0), int(x1), int(y1), id = tracker_id, font_scale = 1.5)
             draw_trasnparent_bbox(frame, int(x0), int(y0), int(x1), int(y1),tracker_id)
+
+        tm.update(track_outputs)
 
     # Show resized frame
     cv2.imshow("yolov8", resize_img(frame, 50))
---
+++ utils/tracker_manager.py
@@ -0,0 +1,24 @@
+class TrackerManager:
+    def __init__(self):
+        self.tracks = {}
+        self.max_missing_frames = 10
+
+    def update(self, tracker_outputs):
+        new_tracks = {}
+        for track_id, bbox in tracker_outputs:
+            if track_id in self.tracks:
+                missing_frames = self.tracks[track_id]['missing_frames']
+                new_tracks[track_id] = {'bbox': bbox, 'missing_frames': max(0, missing_frames - 1)}
+            else:
+                new_tracks[track_id] = {'bbox': bbox, 'missing_frames': 0}
+
+        for track_id in self.tracks:
+            if track_id not in new_tracks:
+                missing_frames = self.tracks[track_id]['missing_frames'] + 1
+                if missing_frames <= self.max_missing_frames:
+                    new_tracks[track_id] = {'bbox': self.tracks[track_id]['bbox'], 'missing_frames': missing_frames}
+
+        self.tracks = new_tracks
+
+    def get_tracks(self):
+        return self.tracks
